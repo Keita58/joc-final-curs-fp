b0d0164edc974b0fb6a855196b3c9315c202f476
Eric Morales Martinez <ericmoralesmartinez77@gmail.com>
2024-05-09 19:28:02 +0200

    Añadido Mixer

--- Joc final programacio/Assets/Scripts/OptionsController.cs
+++ Joc final programacio/Assets/Scripts/OptionsController.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class OptionsController : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
---
+++ Joc final programacio/Assets/VolumeSettings.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeSettings : MonoBehaviour
+{
+  [SerializeField] private AudioMixer mixer;
+  [SerializeField] private Slider musicSlider;
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            SetMusicVolume();
+        }
+    }
+    public void SetMusicVolume()
+    {
+        float volume = musicSlider.value;
+        mixer.SetFloat("music", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+    }
+    public void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        SetMusicVolume();
+    }
+}

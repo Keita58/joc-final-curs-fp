ddbd92f683011276e4c7998c01cc75a155622599
Marc Sánchez <marc-s-26@hotmail.com>
2024-05-15 17:59:53 +0200

    Feina

--- Joc final programacio/Assets/Scripts/Enemic.cs
+++ Joc final programacio/Assets/Scripts/Enemic.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Enemic : MonoBehaviour
 {
     [SerializeField] int casellesMoviment;
+    [SerializeField] GameObject objectiu;
     // Start is called before the first frame update
     void Start()
     {
---
+++ Joc final programacio/Assets/Scripts/Grid.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Grid : MonoBehaviour
+{
+    [SerializeField] Tilemap tilemapGround;
+    [SerializeField] Tilemap tilemapDifficult;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
